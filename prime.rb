6cd349a263ce85f21685fcb0cc1dd739497e1ded
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-04-28 05:20:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+def prime?(integer)
+  count = 0 
+  i = 1 
+  
+  if integer < 0 
+    integer *= -1
+  end 
+  if integer == 1 || integer == 0  
+    return false
+  end 
+  while i <= integer
+    if integer%i == 0 
+      count += 1 
+      if count == 3
+        return false
+      end
+    end
+    i += 1 
+  end
+    return true
+end
