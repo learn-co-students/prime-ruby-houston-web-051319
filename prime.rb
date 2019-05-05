393a5185791cc2e2751673d96e2c2cf1d48d2880
Brendan Brown <pbbrown1112@gmail.com>
2019-05-04 19:45:02 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+  range = (1...num)
+  arr1 = range.to_a
+  arr1.shift()
+  if num <= 1
+    return false
+  elsif num == 2 || num == 3
+    return true
+  elsif
+    arr1.each do |i|
+      if num%i == 0
+        return false
+      end
+    end
+    return true
+  end
+end
