c40c21bd282fbf6413aee088aad331c4d85c1bfd
BobNarwhalKing <danceflurry4366@gmail.com>
2019-05-14 07:19:32 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(integer)
+    test_array = Array(1..integer)
+    test_array.shift
+    test_array.pop
+    result_array = []
+    test_array.each do |x|
+        result_array.push(integer % x)
+    end
+    if integer <= 1
+        return false
+    elsif result_array.delete(0) == nil
+        return true
+    else
+        return false
+    end
+end
